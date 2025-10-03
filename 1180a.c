d6f660c8c01c0caf79468f5dc9f75cb444aedee9
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2025-10-02 22:28:20 -0300

    1180a.c

--- 1180a.c
+++ 1180a.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+
 int main() {
-    int n;
-    int x[1000];
-    int menor;
-    int posicao = 0;
-    printf("DIGITE O NUMERO DE ELEMENTOS (ENTRE 1 E 1000): ");
-    scanf("%d", &n);
-    while (n < 1 || n > 1000) {
-        printf("VALOR INVALIDO. DIGITE UM VALOR ENTRE 1 E 1000: ");
-        scanf("%d", &n);
+    int N;
+    scanf("%d", &N);
+
+    int X[N];
+    int menor_valor;
+    int posicao;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &X[i]);
     }
-    printf("DIGITE OS %d VALORES PARA O VETOR:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &x[i]);
-    }
-    menor = x[0];
-    for (int i = 1; i < n; i++) {
-        if (x[i] < menor) {
-            menor = x[i];
+
+    menor_valor = X[0];
+    posicao = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (X[i] < menor_valor) {
+            menor_valor = X[i];
             posicao = i;
         }
     }
-    printf("Menor valor: %d\n", menor);
-    printf("Posicao: %d\n", posicao + 1);
+
+    printf("Menor valor: %d\n", menor_valor);
+    printf("Posicao: %d\n", posicao);
+
     return 0;
 }
