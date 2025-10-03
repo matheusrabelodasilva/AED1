2f0a3797b9a990b780e15db5590eb1f51db0e3a0
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2025-10-02 22:29:57 -0300

    1180c.c

---
+++ 1180c.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int *X;
+    X = (int*) malloc(N * sizeof(int));
+    
+    if (X == NULL) {
+        return 1; 
+    }
+
+    int menor_valor;
+    int posicao;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &X[i]);
+    }
+
+    menor_valor = X[0];
+    posicao = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (X[i] < menor_valor) {
+            menor_valor = X[i];
+            posicao = i;
+        }
+    }
+
+    printf("Menor valor: %d\n", menor_valor);
+    printf("Posicao: %d\n", posicao);
+
+    free(X);
+
+    return 0;
+}
