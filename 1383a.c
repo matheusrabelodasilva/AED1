e9949867e7c65c384a76baa5ad25cd6a64c76163
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2025-10-02 22:35:39 -0300

    1383a.c

--- 1383a.c
+++ 1383a.c
@@ -1,81 +1,64 @@
 #include <stdio.h>
-
-int verifica_bloco(int bloco[9]) {
-    int vistos[10] = {0};
-    int i;
-    int num;
-
-    for (i = 0; i < 9; i++) {
-        num = bloco[i];
-        if (num < 1 || num > 9 || vistos[num]) {
+int verifica_grupo(int grupo[9]) {
+    int visto[10] = {0}; 
+    for (int i = 0; i < 9; i++) {
+        int num = grupo[i];
+        if (num < 1 || num > 9 || visto[num] == 1) {
             return 0; 
         }
-        vistos[num] = 1; 
+        visto[num] = 1; 
     }
-    return 1; 
-}
-
-int eh_solucao_valida(int matriz[9][9]) {
-    int i, j, k;
-    int i_bloco, j_bloco;
-    
-    for (i = 0; i < 9; i++) {
-        if (!verifica_bloco(matriz[i])) {
-            return 0;
-        }
-    }
-
-    for (j = 0; j < 9; j++) {
-        int coluna[9];
-        for (i = 0; i < 9; i++) {
-            coluna[i] = matriz[i][j];
-        }
-        if (!verifica_bloco(coluna)) {
-            return 0;
-        }
-    }
-
-    for (i_bloco = 0; i_bloco < 9; i_bloco += 3) {
-        for (j_bloco = 0; j_bloco < 9; j_bloco += 3) {
-            int sub_matriz[9];
-            k = 0;
-            for (i = i_bloco; i < i_bloco + 3; i++) {
-                for (j = j_bloco; j < j_bloco + 3; j++) {
-                    sub_matriz[k++] = matriz[i][j];
-                }
-            }
-            if (!verifica_bloco(sub_matriz)) {
-                return 0;
-            }
-        }
-    }
-
     return 1;
 }
-
 int main() {
-    int n;
-    int k;
-    int i, j;
-    int matriz[9][9];
-
+    int n, instancia;
     scanf("%d", &n);
-
-    for (k = 1; k <= n; k++) {
-        for (i = 0; i < 9; i++) {
-            for (j = 0; j < 9; j++) {
+    for (instancia = 1; instancia <= n; instancia++) {
+        int matriz[9][9];
+        int solucao_valida = 1;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 scanf("%d", &matriz[i][j]);
             }
         }
-
-        printf("Instancia %d\n", k);
-        if (eh_solucao_valida(matriz)) {
-            printf("SIM\n");
-        } else {
-            printf("NAO\n");
+        for (int i = 0; i < 9; i++) {
+            int linha[9];
+            int coluna[9];
+            for (int j = 0; j < 9; j++) {
+                linha[j] = matriz[i][j];
+                coluna[j] = matriz[j][i];
+            }
+            if (!verifica_grupo(linha) || !verifica_grupo(coluna)) {
+                solucao_valida = 0;
+                break;
+            }
         }
-        printf("\n");
+        if (!solucao_valida) {
+            printf("Instancia %d\nNAO\n\n", instancia);
+            continue;
+        }
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                int submatriz[9];
+                int k = 0;
+                for (int sub_i = 0; sub_i < 3; sub_i++) {
+                    for (int sub_j = 0; sub_j < 3; sub_j++) {
+                        submatriz[k++] = matriz[i + sub_i][j + sub_j];
+                    }
+                }
+                if (!verifica_grupo(submatriz)) {
+                    solucao_valida = 0;
+                    break;
+                }
+            }
+            if (!solucao_valida) break;
+        }
+        printf("Instancia %d\n", instancia);
+        if (solucao_valida) {
+            printf("SIM\n\n");
+        } else {
+            printf("NAO\n\n");
+        }
     }
-
     return 0;
 }
