4bafa9cfbc8c42a773b5c435046f48489e60a0d3
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-04 17:14:21 -0300

    Update 1152.c

--- 1152.c
+++ 1152.c
@@ -1,62 +1,52 @@
+//dijkstra, exercicio da guerra 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAX_N 200005
-
+#define MAX_ARESTAS 200005
 typedef struct {
     int u, v, peso;
-} Aresta;
-
-Aresta arestas[MAX_N];
-int pai[MAX_N];
-
+} Estrada;
+Estrada lista[MAX_ARESTAS];
+int grupos[MAX_ARESTAS];
+// Função para ordenar as estradas da mais barata para a mais cara
 int comparar(const void *a, const void *b) {
-    return ((Aresta*)a)->peso - ((Aresta*)b)->peso;
+    return ((Estrada*)a)->peso - ((Estrada*)b)->peso;
 }
-
-int encontrar(int i) {
-    if (pai[i] == i)
+// Função que descobre a qual grupo uma cidade pertence
+int buscar_grupo(int i) {
+    if (grupos[i] == i)
         return i;
-    return pai[i] = encontrar(pai[i]);
+    return grupos[i] = buscar_grupo(grupos[i]); // Otimização: aponta direto para o chefe do grupo
 }
-
-void unir(int i, int j) {
-    int raiz_i = encontrar(i);
-    int raiz_j = encontrar(j);
-    if (raiz_i != raiz_j) {
-        pai[raiz_i] = raiz_j;
-    }
-}
-
 int main() {
-    int m, n;
-    int i;
-    
+    int m, n;   
     while (scanf("%d %d", &m, &n) && (m != 0 || n != 0)) {
-        int custo_total = 0;
-        
-        for (i = 0; i < n; i++) {
-            scanf("%d %d %d", &arestas[i].u, &arestas[i].v, &arestas[i].peso);
-            custo_total += arestas[i].peso;
+        long long custo_total_original = 0; // Usamos long long porque o valor pode ser alto
+        for (int i = 0; i < n; i++) {
+            scanf("%d %d %d", &lista[i].u, &lista[i].v, &lista[i].peso);
+            custo_total_original += lista[i].peso;
         }
-
-        qsort(arestas, n, sizeof(Aresta), comparar);
-
-        for (i = 0; i < m; i++) {
-            pai[i] = i;
+        // 1. Ordenamos para priorizar as estradas mais baratas
+        qsort(lista, n, sizeof(Estrada), comparar);
+        // 2. Inicialmente, cada cidade é seu próprio grupo
+        for (int i = 0; i < m; i++) {
+            grupos[i] = i;
         }
-
-        int custo_mst = 0;
-        
-        for (i = 0; i < n; i++) {
-            if (encontrar(arestas[i].u) != encontrar(arestas[i].v)) {
-                unir(arestas[i].u, arestas[i].v);
-                custo_mst += arestas[i].peso;
+        long long custo_minimo_conectar = 0;
+        int conexoes_feitas = 0;
+        // 3. Percorremos as estradas baratas e vamos unindo as cidades
+        for (int i = 0; i < n; i++) {
+            int raiz_u = buscar_grupo(lista[i].u);
+            int raiz_v = buscar_grupo(lista[i].v);
+            // Se as cidades estão em grupos diferentes, precisamos dessa estrada!
+            if (raiz_u != raiz_v) {
+                grupos[raiz_u] = raiz_v; // Une os grupos
+                custo_minimo_conectar += lista[i].peso;
+                conexoes_feitas++;
             }
+            // Se já estão no mesmo grupo, ignoramos a estrada (economia!)
         }
-
-        printf("%d\n", custo_total - custo_mst);
+        // A resposta é a diferença entre o que tínhamos e o que realmente precisamos
+        printf("%lld\n", custo_total_original - custo_minimo_conectar);
     }
-
     return 0;
 }
