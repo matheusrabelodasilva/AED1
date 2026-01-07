82211036fb8849f7f2e653777fa62eee6312d50e
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-07 20:33:32 -0300

    Update 1907.c

--- 1907.c
+++ 1907.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
-
 #define MAX 1025
-
 char grid[MAX][MAX];
 int N, M;
-
 typedef struct {
     int x, y;
 } Ponto;
-
 Ponto fila[MAX * MAX];
-
 void bfs(int i, int j) {
     int inicio = 0, fim = 0;
-    
     fila[fim].x = i;
     fila[fim].y = j;
     fim++;
-    
     grid[i][j] = 'o'; 
-
     int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
-
     while (inicio < fim) {
         Ponto atual = fila[inicio];
         inicio++;
-
         for (int k = 0; k < 4; k++) {
             int nx = atual.x + dx[k];
             int ny = atual.y + dy[k];
-
             if (nx >= 0 && nx < N && ny >= 0 && ny < M && grid[nx][ny] == '.') {
                 grid[nx][ny] = 'o';
                 fila[fim].x = nx;
@@ -40,17 +29,12 @@ void bfs(int i, int j) {
         }
     }
 }
-
 int main() {
-    // O problema no Beecrowd geralmente pode ter multiplos casos de teste ou apenas um
-    // O padrao com EOF cobre ambos os cenarios
     while (scanf("%d %d", &N, &M) != EOF) {
         for (int i = 0; i < N; i++) {
             scanf("%s", grid[i]);
         }
-
         int clicks = 0;
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (grid[i][j] == '.') {
@@ -59,9 +43,7 @@ int main() {
                 }
             }
         }
-
         printf("%d\n", clicks);
     }
-
     return 0;
 }
