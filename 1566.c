f1a31b5f26b16a428355bc223e9d793adccf3d20
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-07 20:10:54 -0300

    Update 1566.c

--- 1566.c
+++ 1566.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-void zera_frequencia(int array[], int tamanho) {
-    memset(array, 0, tamanho * sizeof(int));
-}
 int main() {
     int num_casos_teste;
-    scanf("%d", &num_casos_teste);
+    if (scanf("%d", &num_casos_teste) != 1) return 0;
     while (num_casos_teste--) {
         int num_cidadaos;
-        scanf("%d", &num_cidadaos);
+        if (scanf("%d", &num_cidadaos) != 1) break;
         int frequencia_alturas[231];
-        zera_frequencia(frequencia_alturas, 231);
+        for (int i = 0; i < 231; i++) {
+            frequencia_alturas[i] = 0;
+        }
         for (int i = 0; i < num_cidadaos; i++) {
             int altura_atual;
             scanf("%d", &altura_atual);
-            if (altura_atual >= 20 && altura_atual <= 230) {
-                frequencia_alturas[altura_atual]++;
-            }
+            frequencia_alturas[altura_atual]++;
         }
         int primeiro_numero_impresso = 1;
         for (int altura = 20; altura <= 230; altura++) {
-            int count = frequencia_alturas[altura];
-            for (int i = 0; i < count; i++) {
+            int quantidade = frequencia_alturas[altura];
+            for (int i = 0; i < quantidade; i++) {
                 if (primeiro_numero_impresso) {
                     printf("%d", altura);
                     primeiro_numero_impresso = 0; 
