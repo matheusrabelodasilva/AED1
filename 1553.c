de632b3e5f0265e7cc512fa69de11c0865765174
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-07 20:07:19 -0300

    Update 1553.c

--- 1553.c
+++ 1553.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
-#include <string.h>
-
 int main() {
     int N, K, P, i, total;
     int frequencia[105];
-
-    while (scanf("%d %d", &N, &K) && (N != 0 || K != 0)) {
-        memset(frequencia, 0, sizeof(frequencia));
-        
+    while (scanf("%d %d", &N, &K) == 2 && (N != 0 || K != 0)) {
+        for (i = 0; i < 105; i++) {
+            frequencia[i] = 0;
+        }
         for (i = 0; i < N; i++) {
             scanf("%d", &P);
             frequencia[P]++;
         }
-
         total = 0;
         for (i = 1; i <= 100; i++) {
             if (frequencia[i] >= K) {
                 total++;
             }
         }
-
         printf("%d\n", total);
     }
-
     return 0;
 }
