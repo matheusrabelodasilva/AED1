279077dd46ebfb1d7a05b804409cc205a2d6f47e
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-07 20:50:31 -0300

    Update 2065.c

--- 2065.c
+++ 2065.c
@@ -1,46 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main() {
     int N, M, i, j;
-    
-    scanf("%d %d", &N, &M);
-    
+    if (scanf("%d %d", &N, &M) != 2) return 0;
     int V[N];
-    int C[M];
-    
-    long long tempo_termino_caixa[N];
-
+    int tempo_termino_caixa[N]; 
     for (i = 0; i < N; i++) {
         scanf("%d", &V[i]);
         tempo_termino_caixa[i] = 0;
     }
-
-    for (j = 0; j < M; j++) {
-        scanf("%d", &C[j]);
-    }
-
     for (i = 0; i < M; i++) {
-        int caixa_livre_idx = 0;
-        
+        int itens_cliente;
+        scanf("%d", &itens_cliente);
+        int caixa_escolhido = 0;
         for (j = 1; j < N; j++) {
-            if (tempo_termino_caixa[j] < tempo_termino_caixa[caixa_livre_idx]) {
-                caixa_livre_idx = j;
-            }
+            if (tempo_termino_caixa[j] < tempo_termino_caixa[caixa_escolhido]) {
+                caixa_escolhido = j;
+            } 
         }
-        
-        tempo_termino_caixa[caixa_livre_idx] += (long long)V[caixa_livre_idx] * C[i];
+        tempo_termino_caixa[caixa_escolhido] += itens_cliente * V[caixa_escolhido];
     }
-
-    long long tempo_maximo = 0;
-    
+    int tempo_total_final = 0;
     for (i = 0; i < N; i++) {
-        if (tempo_termino_caixa[i] > tempo_maximo) {
-            tempo_maximo = tempo_termino_caixa[i];
+        if (tempo_termino_caixa[i] > tempo_total_final) {
+            tempo_total_final = tempo_termino_caixa[i];
         }
     }
-
-    printf("%lld\n", tempo_maximo);
-
+    printf("%d\n", tempo_total_final);
     return 0;
 }
