509dabe639a8fd4af11baa510150b451bc393fd3
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-07 20:56:30 -0300

    Update 2653.c

--- 2653.c
+++ 2653.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 typedef struct no {
     char *palavra;
     struct no *esq;
     struct no *dir;
 } No;
-
 unsigned int total_palavras = 0;
-
+void copiar_string(char *destino, char *origem) {
+    int i = 0;
+    while (origem[i] != '\0') {
+        destino[i] = origem[i];
+        i++;
+    }
+    destino[i] = '\0'; 
+}
 No* inserir(No *raiz, char *texto) {
-    int comparacao;
-
     if (raiz == NULL) {
         raiz = (No*)malloc(sizeof(No));
-        // Aloca apenas o espaco necessario para a string
         raiz->palavra = (char*)malloc(strlen(texto) + 1);
-        strcpy(raiz->palavra, texto);
+        copiar_string(raiz->palavra, texto);       
         raiz->esq = NULL;
         raiz->dir = NULL;
         total_palavras++;
     } else {
-        comparacao = strcmp(raiz->palavra, texto);
-        
-        if (comparacao > 0) {
+        int comparacao = strcmp(texto, raiz->palavra);
+        if (comparacao < 0) {
             raiz->esq = inserir(raiz->esq, texto);
-        } else if (comparacao < 0) {
+        } else if (comparacao > 0) {
             raiz->dir = inserir(raiz->dir, texto);
         }
     }
     return raiz;
 }
-
 void liberar(No *raiz) {
     if (raiz != NULL) {
         liberar(raiz->esq);
@@ -41,18 +41,13 @@ void liberar(No *raiz) {
         free(raiz);
     }
 }
-
 int main() {
     char buffer[100010]; 
     No *arvore = NULL;
-
     while (scanf("%s", buffer) != EOF) {
         arvore = inserir(arvore, buffer);
     }
-
     printf("%u\n", total_palavras);
-
     liberar(arvore);
-
     return 0;
 }
