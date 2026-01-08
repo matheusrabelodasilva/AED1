e8549c9037cff6d25ef4aec83a323bb20b92a8b6
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-08 11:54:53 -0300

    Update 1082.c

--- 1082.c
+++ 1082.c
@@ -1,79 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 int matriz[30][30];
 int visitado[30];
-int lista_vertices[30];
-int tam_lista;
 int total_vertices;
-
-int comparar(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-
-void busca_profundidade(int u) {
-    int v;
-    visitado[u] = 1;
-    lista_vertices[tam_lista++] = u;
-    
-    for (v = 0; v < total_vertices; v++) {
+void busca_marcar(int u) {
+    visitado[u] = 1; 
+    for (int v = 0; v < total_vertices; v++) {
         if (matriz[u][v] == 1 && visitado[v] == 0) {
-            busca_profundidade(v);
+            busca_marcar(v);
         }
     }
 }
-
 int main() {
     int casos, caso_atual = 1;
     int arestas, i, j;
     char origem_c, destino_c;
-    int origem, destino;
-    int componentes;
-
-    scanf("%d", &casos);
-
-    while (casos > 0) {
-        scanf("%d %d", &total_vertices, &arestas);
-
+    if (scanf("%d", &casos) != 1) return 0;
+    while (caso_atual <= casos) {
+        if (scanf("%d %d", &total_vertices, &arestas) != 2) break;
         for (i = 0; i < total_vertices; i++) {
             visitado[i] = 0;
             for (j = 0; j < total_vertices; j++) {
                 matriz[i][j] = 0;
             }
         }
-
         for (i = 0; i < arestas; i++) {
             scanf(" %c %c", &origem_c, &destino_c);
-            origem = origem_c - 'a';
-            destino = destino_c - 'a';
-            matriz[origem][destino] = 1;
-            matriz[destino][origem] = 1;
+            int u = origem_c - 'a';
+            int v = destino_c - 'a';
+            matriz[u][v] = 1;
+            matriz[v][u] = 1;
         }
-
         printf("Case #%d:\n", caso_atual);
-        componentes = 0;
-
+        int componentes = 0;
         for (i = 0; i < total_vertices; i++) {
             if (visitado[i] == 0) {
                 componentes++;
-                tam_lista = 0;
-                busca_profundidade(i);
-                
-                qsort(lista_vertices, tam_lista, sizeof(int), comparar);
-
-                for (j = 0; j < tam_lista; j++) {
-                    printf("%c,", lista_vertices[j] + 'a');
+                int jah_visitado_antes[30];
+                for(int k=0; k<total_vertices; k++) jah_visitado_antes[k] = visitado[k];
+                busca_marcar(i);
+                for (int k = 0; k < total_vertices; k++) {
+                    if (jah_visitado_antes[k] == 0 && visitado[k] == 1) {
+                        printf("%c,", k + 'a');
+                    }
                 }
                 printf("\n");
             }
         }
-
         printf("%d connected components\n\n", componentes);
-        
         caso_atual++;
-        casos--;
     }
-
     return 0;
 }
