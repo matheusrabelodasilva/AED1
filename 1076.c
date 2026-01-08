206ff8593be87776b15f17bfa10f33321092d1ee
matheusrabelodasilva <matheus.rabelo@unifesp.br>
2026-01-08 11:49:34 -0300

    Update 1076.c

--- 1076.c
+++ 1076.c
@@ -1,23 +1,20 @@
 //grafo simples nao direcionado com matriz de adjacencia 
 #include <stdio.h>
-#include <string.h>
-
 int main() {
     int testes, inicio, V, A, u, v, movimentos;
     int grafo[50][50];
-
-    scanf("%d", &testes);
-
+    if (scanf("%d", &testes) != 1) return 0;
     while (testes--) {
         scanf("%d", &inicio);
         scanf("%d %d", &V, &A);
-
-        memset(grafo, 0, sizeof(grafo));// limpa o grafo para analisarmos as proximas arestas
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 50; j++) {
+                grafo[i][j] = 0;
+            }
+        }
         movimentos = 0;
-
         while (A--) {
             scanf("%d %d", &u, &v);
-            
             if (grafo[u][v] == 0) {
                 grafo[u][v] = 1;
                 grafo[v][u] = 1;
@@ -27,6 +24,5 @@ int main() {
 
         printf("%d\n", movimentos);
     }
-
     return 0;
 }
